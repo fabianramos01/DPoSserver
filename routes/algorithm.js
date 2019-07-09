1ca9d2827b461b2d8b76edb5242343ad26245bd1
fabianramos01 <fabian.ramos01@uptc.edu.co>
2019-07-08 23:11:24 -0500

    Metodo para asignar la votación de cada cliente

--- routes/algorithm.js
+++ routes/algorithm.js
@@ -6,6 +6,7 @@ module.exports = function(server, app) {
     const clientIO = io.of('/client');
 
     let nodeNum = 0;
+    let votes = 0;
     let minNum = 4;
     let nodes = [];
     let servers = [];
@@ -13,6 +14,7 @@ module.exports = function(server, app) {
 
     serverIO.on('connection', (socket) => {
         servers.push(socket.id);
+        serverIO.emit('nodeList', {'list': nodes});
         console.log(`Servidor conectado con id: ${socket.id}`);
 
         socket.on('initiateVoting', () => {
@@ -25,9 +27,16 @@ module.exports = function(server, app) {
     clientIO.on('connection', (socket) => {
         nodes.push(new Node(socket.id, socket.request.connection.remoteAddress, socket.request.connection.remotePort));
         console.log(`Usuario conectado con id: ${socket.id}`);
+        serverIO.emit('nodeList', {'list': nodes});
 
-        socket.on('clientHour', (data) => {
-
+        socket.on('vote', (data) => {
+            for (let i = 0; i < nodes.length; i++) {
+                if (node.id === data.id) {
+                    nodes[i].votes  += data.stake;
+                    break;
+                }
+            }
+            votes++;
         });
     });
 
@@ -40,5 +49,6 @@ module.exports = function(server, app) {
         this.ip = ip;
         this.port = port;
         this.votes = 0;
+        this.stack =  Math.random() * (6 - 1) + 1;
     }
 };
